c83fc693459625b4d2bddf5e1e7f5593dfb6f336
bikash <iamcrazy15@hotmail.com>
2018-01-23 11:15:35 +0530

    All Paging Fix

--- Vatas_UI/Process/Process_AssignReturns.aspx.cs
+++ Vatas_UI/Process/Process_AssignReturns.aspx.cs
@@ -182,7 +182,10 @@ namespace Vatas_UI.Process
 
             int PageSize = 10;
             int.TryParse(ddlPageLength.SelectedValue, out PageSize);
-
+            if (PageSize <= 0)
+            {
+                PageSize = 2500000;
+            }
             string SearchText = txtSearch.Text.Trim();
 
             BindData(CurrentPageNo, PageSize, SearchText);
